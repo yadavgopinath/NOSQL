6e65a4c26f1e6bbc631b6f220c22b3130e82bab1
yadavgopinath <yadavgopinath93@gmail.com>
2025-03-09 00:40:50 +0530

    Extra features of Cart System

--- controllers/shop.js
+++ controllers/shop.js
@@ -104,8 +104,9 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
+  console.log("Product ID received:", prodId); // Debugging
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
--- models/user.js
+++ models/user.js
@@ -46,6 +46,16 @@ return this.save();
 
 };
 
+userSchema.methods.removeFromCart = function(productId){
+  console.log("Before Deletion:", this.cart.items); // Debugging
+    const updatedCartItems= this.cart.items.filter(item=>{
+      return item.productId.toString()!==productId.toString();
+    });
+    console.log("After Deletion:", updatedCartItems); // Debugging
+    this.cart.items=updatedCartItems;
+    return this.save();
+  };
+
 
 
 module.exports = mongoose.model('User', userSchema);
